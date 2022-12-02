3cb0789735b3ac2ce3a964ec873b6ad182f20e19
ghazal <ahmed.ghazal365@gmail.com>
2022-12-02 20:45:44 +0200

    shopping cart screen

--- lib/features/posts/presentation/pages/profile_page/shopping_cart_page.dart
+++ lib/features/posts/presentation/pages/profile_page/shopping_cart_page.dart
@@ -1,10 +1,9 @@
 import 'package:auction_clean_architecture/core/strings/failures.dart';
+import 'package:auction_clean_architecture/features/auction_event/cubit/cubit.dart';
 import 'package:auction_clean_architecture/features/posts/presentation/widget/post_card_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../authentication/cubit/auth_methoed.dart';
 
 class ShoppingCartPage extends StatefulWidget {
   const ShoppingCartPage({super.key});
@@ -27,70 +26,102 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
   late var userModel;
   @override
   void initState() {
-    userModel = AuthCubit.get(context).userData;
+    userModel = AuctionCubit.get(context).userData;
     Net();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    print(userModel.uid);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Shopping Cart',
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            net
-                ? FutureBuilder(
-                    future: FirebaseFirestore.instance
-                        .collection('posts')
-                        .where('winner', isEqualTo: userModel.uid)
-                        .where('enddate', isLessThan: DateTime.now())
-                        .get(),
-                    builder: (context,
-                        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                            snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
+      body: Stack(
+        children: [
+          Container(
+            height: size.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("asset/image/222.jpg"),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                net
+                    ? StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('posts')
+                            .where('winnerID', isEqualTo: userModel.uid)
+                            .where('enddate', isLessThan: DateTime.now())
+                            .snapshots(),
+                        builder: (context,
+                            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                                snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          if (!snapshot.hasData) {
+                            return Center(
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    SizedBox(height: 30),
+                                    Icon(Icons.remove_shopping_cart_outlined,
+                                        size: 100),
+                                    SizedBox(height: 30),
+                                    Text(
+                                      "you don't have any items in shopping cart",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 100)
+                                  ]),
+                            );
+                          }
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: (snapshot.data! as dynamic).docs.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot snap =
+                                  (snapshot.data! as dynamic).docs[index];
+                              return Postcard(snap: snap);
+                            },
+                          );
+                        })
+                    : Center(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.wifi_off,
+                                size: 50,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Text(OFFLINE_FAILURE_MESSAGE),
+                              SizedBox(
+                                height: 100,
+                              )
+                            ]),
+                      ),
 
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: (snapshot.data! as dynamic).docs.length,
-                        itemBuilder: (context, index) {
-                          DocumentSnapshot snap =
-                              (snapshot.data! as dynamic).docs[index];
-
-                          return Postcard(snap: snap);
-                        },
-                      );
-                    })
-                : Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.wifi_off,
-                            size: 50,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Text(OFFLINE_FAILURE_MESSAGE),
-                          SizedBox(
-                            height: 100,
-                          )
-                        ]),
-                  ),
-
-            //
-          ],
-        ),
+                //
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
